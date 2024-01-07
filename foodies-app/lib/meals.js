3a72f168f8942b14434d071c3f73be5a4d2cc57d
bbjbc <jobc4550@gmail.com>
2024-01-07 23:00:45 +0900

    feat: add meals-detail page & add sql to get detailed info through params

--- foodies-app/lib/meals.js
+++ foodies-app/lib/meals.js
@@ -4,7 +4,13 @@ const db = sql("meals.db");
 
 export async function getMeals() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  // throw new Error("Loading meals failed");
   return db.prepare("SELECT * FROM meals").all();
   // run()은 데이터를 주입시킬 때, 데이터를 바꿀 때 사용함
   // all()은 데이터를 가져올 때, 이 문장에 의해 가져오는 모든 행을 가져올 때 사용함
+}
+
+export function getMeal(slug) {
+  return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
 }
