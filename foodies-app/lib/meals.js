4ffe44ae32414647dba0b39c4d4036d2ab0376bc
bbjbc <jobc4550@gmail.com>
2024-01-07 21:17:28 +0900

    feat: fetch data using full stack function in next.js

---
+++ foodies-app/lib/meals.js
@@ -0,0 +1,10 @@
+import sql from "better-sqlite3";
+
+const db = sql("meals.db");
+
+export async function getMeals() {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  return db.prepare("SELECT * FROM meals").all();
+  // run()은 데이터를 주입시킬 때, 데이터를 바꿀 때 사용함
+  // all()은 데이터를 가져올 때, 이 문장에 의해 가져오는 모든 행을 가져올 때 사용함
+}
