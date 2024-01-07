e8b18333710371362528946cdcfcef2b24223373
bbjbc <jobc4550@gmail.com>
2024-01-07 17:58:32 +0900

    feat: add meals-grid comp and meal-item comp & refactor meals page insert empty data

--- foodies-app/app/meals/page.js
+++ foodies-app/app/meals/page.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import classes from "./page.module.css";
 
+import MealsGrid from "@/components/meals/meals-grid";
+
 export default function MealsPage() {
   return (
     <>
@@ -16,7 +18,9 @@ export default function MealsPage() {
           <Link href="/meals/share">Share Your Favorite Recipe</Link>
         </p>
       </header>
-      <main className={classes.main}></main>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
     </>
   );
 }
