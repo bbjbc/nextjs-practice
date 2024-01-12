3e073b3172ac53ae4b7e7b32bbad63be66526a57
bbjbc <jobc4550@gmail.com>
2024-01-12 18:51:52 +0900

    refactor: pre-rendering data through file system & incremental static generation through revalidate

--- data-fetching-app/pages/index.js
+++ data-fetching-app/pages/index.js
@@ -1,3 +1,6 @@
+import path from "path";
+import fs from "fs/promises";
+
 export default function HomePage({ products }) {
   return (
     <ul>
@@ -9,10 +12,18 @@ export default function HomePage({ products }) {
 }
 
 export async function getStaticProps() {
+  // console.log("re-generating"); // 개발서버에서는 새로고침 시 마다 생성되지만, 빌드 이후에는 10초가 지나야만 뜸.
+  const filePath = path.join(process.cwd(), "data", "dummy_backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
   return {
     props: {
-      products: [{ id: "p1", title: "Product 1" }],
+      products: data.products,
     },
+    revalidate: 10,
+    // 페이지로 들어오는 모든 요청에 대해 마지막으로 재생성된 지 10초가 지나면 재생성 되어야 한다고 말하는 것임.
+    // 자동 데이터 재빌드 느낌
   };
 }
 // 위 함수는 클라이언트 사이드에서 실행되지 않음. 위 함수가 실행된 후 HomePage 함수가 실행됨.
