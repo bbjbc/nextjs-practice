3c7d42928aebe72a069f8587cfeb5f60e3686996
bbjbc <jobc4550@gmail.com>
2024-01-15 22:42:26 +0900

    refactor: combine pre-rendering with client side fetching

--- data-fetching-app/pages/last-sales.js
+++ data-fetching-app/pages/last-sales.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 
-export default function LastSalesPage() {
-  const [sales, setSales] = useState();
+export default function LastSalesPage({ sale }) {
+  const [sales, setSales] = useState(sale);
   //   const [isLoading, setIsLoading] = useState(false);
 
   const { data, error } = useSWR(
@@ -58,7 +58,7 @@ export default function LastSalesPage() {
     return <p>Failed to load.</p>;
   }
 
-  if (!data || !sales) {
+  if (!data && !sales) {
     return <p>Loading...</p>;
   }
 
@@ -71,6 +71,30 @@ export default function LastSalesPage() {
       ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://nextjs-course-faa3e-default-rtdb.firebaseio.com/sales.json"
+  );
+  const data = await response.json();
+
+  const transformedSales = [];
+
+  for (const key in data) {
+    transformedSales.push({
+      id: key,
+      username: data[key].username,
+      volume: data[key].volume,
+    });
+  }
+
+  return {
+    props: {
+      sale: transformedSales,
+    },
+    revalidate: 10,
+  };
 }
 
 // useSWR()훅 - fetch API 사용. 캐싱 및 자동 유효성 재검사, 에러 시 요청 재시도, 코드 전체를 직접 작성하지 않아도 됨.
